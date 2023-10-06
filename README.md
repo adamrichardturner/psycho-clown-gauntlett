1032beb3bcd5b801daa9915dcafef772621c5219
adamrichardturner <hello@adamrichardturner.dev>
2023-10-06 04:20:08 +0100

    Updated game and readme

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# wrestling-clowns-game
-A little fun making my first HTML Canvas game
+# Psycho Clowns Gauntlet
+
+![](clown-cheerleaders.png)
+
+## Storyline
+
+In a world where societal norms and stereotypical beauty standards run rampant, a teenage girl (she/her) with a confused identity embarks on an unusual journey.
+
+Dreaming of a double career as a wrestler and a cheerleader, she is fueled by passion and determination.
+
+However, her dreams come crashing down when she fails at her cheerleading endeavors - not because of a lack of skill, but because she didn't fit the mold of what society considered "pretty enough", rather she is too masculine.
+
+Represented in the game by a 'baby boy emoji', she is not deterred by this setback.
+
+Instead, she faces an even bigger challenge - a gauntlet of psycho clowns.
+
+These menacing clowns stand between her and the trophy she rightly deserves - a trophy she would have easily won, had the world seen her true worth beyond superficial beauty standards.
+
+But she is not one to be easily defeated.
+
+With 3 lives in hand, she is ready to tackle the challenges ahead, needing only 3 points to claim her victory and prove her worth to the world.
+
+## Game Features
+
+- **3 Lives:** Players start the game with three lives, representing their chances to navigate through the challenges and secure the coveted trophy.
+- **Psycho Clowns:** Five clowns with varying abilities, from patrolling a fixed route to predicting your next move, stand in your way. These clowns are not just obstacles, but a representation of societal prejudices and stereotypes.
+- **The Trophy:** Representing her dreams and aspirations, the trophy is the ultimate goal. Collect it thrice and win the game, proving to the world the irrelevance of superficial judgments.
+
+## Technology Stack
+
+- **Phaser 3:** The game is built using Phaser 3, a fast, free, and fun open source framework for Canvas and WebGL powered browser games.
+- **Arcade Physics:** Provides a simple physics system for the game, allowing for movement, collisions, and other game dynamics.
+- **Keyboard Controls:** Allows the player to control the 'baby boy emoji' using arrow keys for navigation.
+- **Dynamic Clown Logic:** Clowns in the game employ unique movement logic, from basic chasing to predictive movement patterns, adding a layer of complexity to the gameplay.
+
+## How to Run
+
+1. Ensure you have a local server setup or a platform that can serve HTML5 games.
+2. Place the game files in the root directory of your server.
+3. Start your server.
+4. Navigate to the game directory in your web browser.
+5. Play and enjoy!
+
+Note: It's essential to run the game on a server due to certain browser restrictions with Phaser when accessing via the `file://` protocol.
+
+---
+
+Embark on this journey of self-discovery, challenge societal norms, and prove that worth is more than just skin deep.
--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// StartScene
+class StartScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'StartScene' })
+  }
+
+  preload() {
+    this.load.image('startBackground', 'startBackground.png')
+  }
+
+  create() {
+    this.background = this.add.image(320, 240, 'startBackground')
+
+    this.input.keyboard.on('keydown', () => {
+      this.scene.start('MainScene')
+    })
+  }
+}
+
 class MainScene extends Phaser.Scene {
   constructor() {
     super({ key: 'MainScene' })
@@ -7,20 +26,26 @@ class MainScene extends Phaser.Scene {
     this.load.image('player', 'baby.png')
     this.load.image('clown', 'clown.png')
     this.load.image('trophy', 'trophy.png')
+    this.load.image('winBackground', 'winBackground.png')
   }
 
   create() {
-    this.player = this.physics.add.sprite(32, 32, 'player').setScale(0.25) // Scale down
-    this.trophy = this.add.image(608, 448, 'trophy').setScale(0.25) // Scale down
+    this.lives = 3
+    this.player = this.physics.add.sprite(32, 32, 'player').setScale(0.25)
+    this.player.setCollideWorldBounds(true)
+    this.trophy = this.physics.add.image(608, 448, 'trophy').setScale(0.25) // Now a physics object
 
     this.clowns = this.physics.add.group()
     for (let i = 0; i < 5; i++) {
-      // Increase count to 5 clowns
       let x = Phaser.Math.Between(100, 540)
       let y = Phaser.Math.Between(100, 380)
-      let clown = this.physics.add.sprite(x, y, 'clown').setScale(0.25) // Scale down
+      let clown = this.physics.add.sprite(x, y, 'clown').setScale(0.25)
       clown.setBounce(1)
       clown.setCollideWorldBounds(true)
+      clown.speed = Phaser.Math.Between(75, 125) // Varying speed for clowns
+      clown.predictive = i < 2 // First 2 clowns have predictive behavior
+      clown.patrol = i >= 2 && i < 4 ? true : false // Two clowns have patrol behavior
+      clown.patrolDir = Math.random() < 0.5 ? -1 : 1 // Direction of patrol (left/right or up/down)
       this.clowns.add(clown)
     }
 
@@ -40,6 +65,28 @@ class MainScene extends Phaser.Scene {
       null,
       this
     )
+
+    this.score = 0
+    this.scoreText = this.add
+      .text(570, 10, 'Score: 0', {
+        fontSize: '16px',
+        fill: '#FFF',
+        fontWeight: 'bold',
+      })
+      .setOrigin(1, 0)
+    this.livesText = this.add.text(10, 10, 'Lives: 3', {
+      fontSize: '16px',
+      fill: '#FFF',
+      fontWeight: 'bold',
+    }) // Display the lives
+
+    this.positionTrophyRandomly()
+  }
+
+  positionTrophyRandomly() {
+    let trophyX = Phaser.Math.Between(50, 590)
+    let trophyY = Phaser.Math.Between(50, 430)
+    this.trophy.setPosition(trophyX, trophyY)
   }
 
   update() {
@@ -59,29 +106,69 @@ class MainScene extends Phaser.Scene {
       this.player.setVelocityY(0)
     }
 
-    // Make the clowns chase the player
+    // Updated clown movement logic
     this.clowns.getChildren().forEach((clown) => {
+      if (clown.patrol) {
+        if (clown.patrolDir == 1) {
+          clown.setVelocity(0, clown.speed) // Vertical patrol
+          if (clown.y > 380) {
+            clown.patrolDir = -1
+          }
+        } else {
+          clown.setVelocity(0, -clown.speed)
+          if (clown.y < 100) {
+            clown.patrolDir = 1
+          }
+        }
+        return
+      }
+
       let dx = this.player.x - clown.x
       let dy = this.player.y - clown.y
-      let dist = Math.sqrt(dx * dx + dy * dy)
 
-      // Normalize the velocity
+      if (clown.predictive) {
+        // Predict the player's direction and adjust dx, dy to intercept
+        dx += this.player.body.velocity.x / 10
+        dy += this.player.body.velocity.y / 10
+      }
+
+      let dist = Math.sqrt(dx * dx + dy * dy)
       let vx = dx / dist
       let vy = dy / dist
-
-      clown.setVelocity(vx * 100, vy * 100) // Multiply by speed, here 100
+      clown.setVelocity(vx * clown.speed, vy * clown.speed) // Use clown's speed
     })
   }
 
   hitClown(player, clown) {
-    this.physics.pause()
-    player.setTint(0xff0000)
-    let gameOver = true
+    this.lives--
+    this.livesText.setText('Lives: ' + this.lives) // Update the displayed lives
+
+    if (this.lives <= 0) {
+      this.physics.pause()
+      player.setTint(0xff0000)
+      this.scene.restart()
+    } else {
+      clown.disableBody(true, true)
+      this.time.delayedCall(2000, () => {
+        let x = Phaser.Math.Between(100, 540)
+        let y = Phaser.Math.Between(100, 380)
+        clown.enableBody(true, x, y, true, true)
+      })
+    }
   }
 
   collectTrophy(player, trophy) {
     trophy.disableBody(true, true)
-    alert('You won!')
+    this.score++
+    this.scoreText.setText('Score: ' + this.score)
+
+    if (this.score >= 3) {
+      this.physics.pause()
+      this.add.image(320, 240, 'winBackground')
+    } else {
+      this.positionTrophyRandomly()
+      trophy.enableBody(true, trophy.x, trophy.y, true, true)
+    }
   }
 }
 
@@ -89,7 +176,7 @@ const config = {
   type: Phaser.AUTO,
   width: 640,
   height: 480,
-  scene: MainScene,
+  scene: [StartScene, MainScene],
   physics: {
     default: 'arcade',
     arcade: {
